7adce3d647d85bec595646fc3d818a691c4d8329
retotrinkler <rt@melonport.com>
2017-06-01 20:01:22 +0200

    Latest contracts and semi dynamic address allocation

--- addressList.js
+++ addressList.js
@@ -1,7 +1,19 @@
-// TODO Addresses are curr case sensitive!
+import ExchangeJson from '/imports/melon/contracts/Exchange.json';
+import UniverseJson from '/imports/melon/contracts/Universe.json';
+import SubscribeJson from '/imports/melon/contracts/Subscribe.json';
+import RedeemJson from '/imports/melon/contracts/Redeem.json';
+import RiskMgmtV1Json from '/imports/melon/contracts/RiskMgmtV1.json';
+import ManagementFeeJson from '/imports/melon/contracts/ManagementFee.json';
+import PerformanceFeeJson from '/imports/melon/contracts/PerformanceFee.json';
+import GovernanceJson from '/imports/melon/contracts/Governance.json';
+import VersionJson from '/imports/melon/contracts/Version.json';
+
+
+const KOVAN_NETWORK_ID = 42;
 
 // Assets
 
+// TODO Addresses are curr case sensitive!
 exports.etherToken = '0x7506c7bfed179254265d443856ef9bda19221cd7';
 exports.melonToken = '0x4dffea52b0b4b48c71385ae25de41ce6ad0dd5a7';
 exports.bitcoinToken = '0x9e4c56a633dd64a2662bdfa69de4fde33ce01bdd';
@@ -14,30 +26,30 @@ exports.cryptoCompare = '0x442Fd95C32162F914364C5fEFf27A0Dc05214706';
 
 // Exchanges
 
-exports.exchange = '0xbecf472bf654c75ba5916ee542bb878699895325';
+exports.exchange = ExchangeJson.networks[KOVAN_NETWORK_ID].address;
 
 // Universe
 
-exports.universe = '0xf5a69abbcd4a257b210fb4420e1b6d24c0f97c0c';
+exports.universe = UniverseJson.networks[KOVAN_NETWORK_ID].address;
 
 // Participation
 
-exports.subscribe = '0x41363981bffbd4139a0b260c24b0e021772e3ab5';
-exports.redeem = '0xa629b92fc98075763fe116281798a544262ac8a6';
+exports.subscribe = SubscribeJson.networks[KOVAN_NETWORK_ID].address;
+exports.redeem = RedeemJson.networks[KOVAN_NETWORK_ID].address;
 
 // Risk Mgmt
 
-exports.riskMgmt = '0xc357b913fb3e3365ec4d9651ce6011289df47986';
+exports.riskMgmt = RiskMgmtV1Json.networks[KOVAN_NETWORK_ID].address;
 
 // Fees
 
-exports.managementFee = '0x9db26eea1af316abafa35d97694b5d36cd9576c8';
-exports.performanceFee = '0xf2e253227ca1485efccbfa62c13a3a0005ea2647';
+exports.managementFee = ManagementFeeJson.networks[KOVAN_NETWORK_ID].address;
+exports.performanceFee = PerformanceFeeJson.networks[KOVAN_NETWORK_ID].address;
 
 // Governance
 
-exports.governance = '0x7bcc9bebbb0fad6a02e555331ffe8004bd7775eb';
-exports.version = '0xcd38c5195f0fe43dbd3a8c8ffb2b836dd1578105';
+exports.governance = GovernanceJson.networks[KOVAN_NETWORK_ID].address;
+exports.version = VersionJson.networks[KOVAN_NETWORK_ID].address;
 
 // Liquidity Provider
 
