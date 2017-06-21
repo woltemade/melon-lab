cce1ba069bb5a4a9881f7de023cbecf2004f465d
Simon Emanuel Schmid <simon@schmid.io>
2017-06-21 11:59:02 +0200

    serializeOrder

---
+++ helpers/serializeOrder.js
@@ -0,0 +1,12 @@
+/*
+  @param order: an order returned by getOrder.js (with howMuch as BigNumber with precision)
+  @returns: howMuch serialized as string (to save into database)
+*/
+const serializeOrder = (order) => {
+  const result = order;
+  result.buy.howMuch = order.buy.howMuch.toString();
+  result.sell.howMuch = order.sell.howMuch.toString();
+  return result;
+};
+
+export default serializeOrder;
