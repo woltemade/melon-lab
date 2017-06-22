38203b833ac5fa1956120c7ca68d51cf72541fe6
Simon Emanuel Schmid <simon@schmid.io>
2017-06-22 17:51:10 +0200

    replace prefill take order with interface/redux

--- helpers/specs.js
+++ helpers/specs.js
@@ -6,7 +6,7 @@ import constants from './constants.js';
 export const getTokenPrecisionByAddress = (address) => {
   if (address === addressList.etherToken) return constants.ETHERTOKEN_PRECISION;
   if (address === addressList.melonToken) return constants.MELONTOKEN_PRECISION;
-  if (address === addressList.bitcoinToken) return constants.BITCOINTOKEN_PRECISION;
+  if (address === addressList.bitcoinToken) { return constants.BITCOINTOKEN_PRECISION; }
   if (address === addressList.euroToken) return constants.EUROTOKEN_PRECISION;
   if (address === addressList.repToken) return constants.REPTOKEN_PRECISION;
   return false;
@@ -27,6 +27,15 @@ export const getTokenAddress = (symbol) => {
   if (symbol === 'BTC-T') return addressList.bitcoinToken;
   if (symbol === 'EUR-T') return addressList.euroToken;
   if (symbol === 'REP-T') return addressList.repToken;
+  return false;
+};
+
+export const getTokenPrecisionBySymbol = (symbol) => {
+  if (symbol === 'ETH-T') return constants.ETHERTOKEN_PRECISION;
+  if (symbol === 'MLN-T') return constants.MELONTOKEN_PRECISION;
+  if (symbol === 'BTC-T') return constants.BITCOINTOKEN_PRECISION;
+  if (symbol === 'EUR-T') return constants.EUROTOKEN_PRECISION;
+  if (symbol === 'REP-T') return constants.REPTOKEN_PRECISION;
   return false;
 };
 
