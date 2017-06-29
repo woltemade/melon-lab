e364b5f4facd4915614b8c600d110fe0788ec5e7
Jenna Zenk <zenkjenna@gmail.com>
2017-06-29 14:53:51 +0200

    update personal stake and value of personal stake w redux

--- getParticipation.js
+++ getParticipation.js
@@ -2,7 +2,11 @@ import contract from 'truffle-contract';
 import web3 from '/imports/lib/web3/client';
 import VaultJson from '@melonproject/protocol/build/contracts/Vault.json';
 
-const getParticipation = async (vaultAddressn managerAddress) => {
+const getParticipation = async (
+  vaultAddress,
+  managerAddress,
+  precision = 18,
+) => {
   const Vault = contract(VaultJson);
   Vault.setProvider(web3.currentProvider);
   const vaultContract = Vault.at(vaultAddress);
@@ -10,7 +14,10 @@ const getParticipation = async (vaultAddressn managerAddress) => {
   const personalStake = await vaultContract.balanceOf(managerAddress);
   const totalSupply = await vaultContract.totalSupply();
 
-  return {personalStake: personalStake, totalSupply};
-}
+  return {
+    personalStake: personalStake.div(Math.pow(10, precision)),
+    totalSupply: totalSupply.div(Math.pow(10, precision)),
+  };
+};
 
 export default getParticipation;
