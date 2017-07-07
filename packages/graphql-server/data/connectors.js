805024ad076c4ed44a67074356cfbe8f4f54b62d
Simon Emanuel Schmid <simon@schmid.io>
2017-07-07 11:59:29 +0200

    refactor into array query

--- packages/graphql-server/data/connectors.js
+++ packages/graphql-server/data/connectors.js
@@ -7,15 +7,19 @@ const provider = new Web3.providers.HttpProvider('https://kovan.melonport.com');
 const web3 = new Web3(provider);
 
 const VersionContract = contract(VersionJson);
+VersionContract.setProvider(web3.currentProvider);
 // const VaultContract = contract(VaultJson);
 
 const Vault = {
   async getById(id) {
-    VersionContract.setProvider(web3.currentProvider);
     const versionInstance = await VersionContract.deployed();
     const vault = await versionInstance.vaults(id);
     const [address, owner, name, symbol, decimals, isActive] = vault;
     return { address, owner, name, symbol, decimals, isActive };
+  },
+  async getLastId() {
+    const versionInstance = await VersionContract.deployed();
+    return versionInstance.getLastVaultId();
   },
 };
 
--- packages/graphql-server/data/resolvers.js
+++ packages/graphql-server/data/resolvers.js
@@ -5,27 +5,18 @@ import { Vault } from './connectors';
 const resolvers = {
   Query: {
     async getVaults(root, { ids }) {
-      // const vault = await Vault.getById(id);
-      const result = await Promise.all(ids.map(async id => await Vault.getById(id)));
-      return result;
+      const lastId = await Vault.getLastId();
+      const queryIds = ids || Array(lastId.toNumber()).fill().map((item, index) => index + 1);
+      const result = await Promise.all(
+        queryIds.map(async id => ({
+          id,
+          ...R.pick(['address', 'owner', 'name', 'symbol'], await Vault.getById(id)),
+        }))
+      );
 
-      // { id: 1, ...R.pick(['address', 'owner', 'name', 'symbol'], vault) };
+      return result;
     },
   },
-  /* Author: {
-    posts(author) {
-      return [
-        { id: 1, title: 'A post', text: 'Some text', views: 2 },
-        { id: 2, title: 'Another post', text: 'Some other text', views: 200 },
-      ];
-    },
-  },
-  Post: {
-    author(post) {
-      return { id: 1, firstName: 'Hello', lastName: 'World' };
-    },
-  },
-  */
 };
 
 export default resolvers;
