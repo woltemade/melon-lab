b58ac3bd4409802e55938dc3cf1530f032d3bd0e
Jenna Zenk <zenkjenna@gmail.com>
2017-07-12 13:22:36 +0200

    fix rep token precision

--- helpers/constants.js
+++ helpers/constants.js
@@ -7,7 +7,7 @@ exports.PREMINED_AMOUNT = new BigNumber(Math.pow(10, 18));
 exports.ETHERTOKEN_PRECISION = 18;
 exports.MELONTOKEN_PRECISION = 18;
 exports.BITCOINTOKEN_PRECISION = 8;
-exports.REPTOKEN_PRECISION = 8; // TODO RepToken has 18 precison! Change at next deployment
+exports.REPTOKEN_PRECISION = 18; // TODO RepToken has 18 precison! Change at next deployment
 exports.EUROTOKEN_PRECISION = 8;
 
 // Price Feed
