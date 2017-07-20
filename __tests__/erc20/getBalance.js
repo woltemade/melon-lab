4e05716b656ddc97aee4f60c84b501c746b11cd2
Jenna Zenk <zenkjenna@gmail.com>
2017-07-20 15:57:02 +0200

    verify that test pass

--- __tests__/erc20/getBalance.js
+++ __tests__/erc20/getBalance.js
@@ -5,11 +5,12 @@ jest.mock('/imports/lib/web3', () => jest.fn(), { virtual: true });
 jest.mock('truffle-contract');
 
 test('getBalance', async () => {
-  const result = await getBalance('0x', '0x');
+  const result = await getBalance('0xToken', '0x1');
 
   expect(result).toBeTruthy();
   expect(result.balanceOf.eq('10')).toBeTruthy();
   expect(contract).toHaveBeenCalledTimes(1);
   expect(contract().setProvider).toHaveBeenCalledTimes(1);
-  expect(contract.mockInspect.instance.balanceOf).toHaveBeenCalledWith('0x');
+  expect(contract().at).toHaveBeenCalledWith('0xToken');
+  expect(contract.mockInspect.instance.balanceOf).toHaveBeenCalledWith('0x1');
 });
