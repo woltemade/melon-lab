7a6f42203eb8849c3ce2aea0886cd14bb03f354f
Simon Emanuel Schmid <simon@schmid.io>
2017-07-27 15:22:50 +0200

    remove some build warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
-// rollup.config.js
+import builtins from "rollup-plugin-node-builtins";
+import globals from "rollup-plugin-node-globals";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import flow from "rollup-plugin-flow";
 import json from "rollup-plugin-json";
 
+import pkg from "./package.json";
+
 export default {
   entry: "lib/main.js",
   format: "cjs",
+  moduleName: "melon.js",
   plugins: [
     flow(),
+    globals({ crypto: true }),
+    builtins({}),
     resolve({
       extensions: [".js", ".json"],
     }),
-    commonjs(),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        "truffle-contract/contract.js": ["default"],
+      },
+    }),
     json(),
     babel({
       presets: [
@@ -26,9 +39,13 @@ export default {
         "stage-0",
         "react",
       ],
+      plugins: ["external-helpers"],
       babelrc: false,
       exclude: "node_modules/**",
     }),
   ],
-  dest: "build/bundle.js",
+  targets: [
+    { dest: pkg.main, format: "umd" },
+    { dest: pkg.module, format: "es" },
+  ],
 };
