7e9a7ef3674d4a07602097ec107914590c585393
Simon Emanuel Schmid <simon@schmid.io>
2017-08-16 18:58:40 +0200

    refactor transferTo

--- tests/mocks/truffle-contract.js
+++ tests/mocks/truffle-contract.js
@@ -33,7 +33,7 @@ const instance = {
   transfer: jest.fn(
     (/* toAddress, quantity { from: fromAddress } */) =>
       new Promise(resolve => {
-        resolve(new BigNumber(3));
+        resolve({ logs: [{ event: "Transfer", args: { id: 1 } }] });
       }),
   ),
   approve: jest.fn(
