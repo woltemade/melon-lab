77e53dfce51bc95cb34d3b313ad527ce93ecd408
Jenna Zenk <zenkjenna@gmail.com>
2017-08-22 17:55:16 +0200

    add getOrderbook function

--- tests/fixtures/blockChainOrders.js
+++ tests/fixtures/blockChainOrders.js
@@ -37,6 +37,78 @@ const orders = [
       true,
     ],
   },
+  {
+    id: 3,
+    data: [
+      new BigNumber(1 * 10 ** 18),
+      "0x2a20ff70596e431ab26C2365acab1b988DA8eCCF", // MLN-T
+      new BigNumber(0.45 * 10 ** 18),
+      "0x1a825E9bF3BdC8ef8B975F97c78b5208a947d0EC", // ETH-T
+      123,
+      "0xDAEMON",
+      true,
+    ],
+  },
+  {
+    id: 4,
+    data: [
+      new BigNumber(0.2 * 10 ** 18),
+      "0x1a825E9bF3BdC8ef8B975F97c78b5208a947d0EC", // ETH-T
+      new BigNumber(1 * 10 ** 18),
+      "0x2a20ff70596e431ab26C2365acab1b988DA8eCCF", // MLN-T
+      123,
+      "0xDAEMON",
+      true,
+    ],
+  },
+  {
+    id: 5,
+    data: [
+      new BigNumber(0.15 * 10 ** 18),
+      "0x1a825E9bF3BdC8ef8B975F97c78b5208a947d0EC", // ETH-T
+      new BigNumber(1 * 10 ** 18),
+      "0x2a20ff70596e431ab26C2365acab1b988DA8eCCF", // MLN-T
+      123,
+      "0xDAEMON",
+      true,
+    ],
+  },
+  {
+    id: 6,
+    data: [
+      new BigNumber(0.1 * 10 ** 18),
+      "0x1a825E9bF3BdC8ef8B975F97c78b5208a947d0EC", // ETH-T
+      new BigNumber(1 * 10 ** 18),
+      "0x2a20ff70596e431ab26C2365acab1b988DA8eCCF", // MLN-T
+      123,
+      "0xDAEMON",
+      true,
+    ],
+  },
+  {
+    id: 7,
+    data: [
+      new BigNumber(1 * 10 ** 18),
+      "0x1a825E9bF3BdC8ef8B975F97c78b5208a947d0EC", // ETH-T
+      new BigNumber(0.1 * 10 ** 18),
+      "0x764ae227ad6a6e143546772169265d76aA9337c6", // BNT-T
+      123,
+      "0xDAEMON",
+      true,
+    ],
+  },
+  {
+    id: 8,
+    data: [
+      new BigNumber(1 * 10 ** 18),
+      "0x1a825E9bF3BdC8ef8B975F97c78b5208a947d0EC", // ETH-T
+      new BigNumber(0.1 * 10 ** 18),
+      "0x2a20ff70596e431ab26C2365acab1b988DA8eCCF", // MLN-T
+      123,
+      "0xDAEMON",
+      false,
+    ],
+  },
 ];
 
 export default orders;
