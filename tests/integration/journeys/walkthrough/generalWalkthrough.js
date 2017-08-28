551fec43ad1f687e328fb7bcb81a1c1934339211
Simon Emanuel Schmid <simon@schmid.io>
2017-08-28 18:15:16 +0200

    WIP: go through subscribe transactions and look for possible dropouts

--- tests/integration/journeys/walkthrough/generalWalkthrough.js
+++ tests/integration/journeys/walkthrough/generalWalkthrough.js
@@ -1,12 +1,15 @@
+import BigNumber from "bignumber.js";
+
 import createVault from "../../../../lib/vault/transactions/createVault";
+import subscribe from "../../../../lib/participation/transactions/subscribe";
 
 const shared = {};
 
 const randomString = (length = 4) =>
   Math.random().toString(36).substr(2, length);
 
-describe("Create fund, invest, take order, redeem", async () => {
-  await it(
+describe("Create fund, invest, take order, redeem", () => {
+  it(
     "createVault",
     async () => {
       shared.vault = await createVault(`test-${randomString()}`);
@@ -14,13 +17,17 @@ describe("Create fund, invest, take order, redeem", async () => {
     30000,
   );
 
-  /*
-  await it(
+  it(
     "invest",
     async () => {
-      shared;
+      console.log(shared);
+      const txHash = await subscribe(
+        shared.vault.address,
+        new BigNumber(1),
+        new BigNumber(1),
+      );
+      console.log(txHash);
     },
-    30000,
+    120000,
   );
-  */
 });
