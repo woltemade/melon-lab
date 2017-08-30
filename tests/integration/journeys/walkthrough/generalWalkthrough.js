cf8bd7f21b6936670d75a714e98e2d62cad2e518
Simon Emanuel Schmid <simon@schmid.io>
2017-08-30 16:47:43 +0200

    walkthrough to takeOrder works (most of the time :|)

--- tests/integration/journeys/walkthrough/generalWalkthrough.js
+++ tests/integration/journeys/walkthrough/generalWalkthrough.js
@@ -1,11 +1,15 @@
 import BigNumber from "bignumber.js";
+import { findLast, propEq } from "ramda";
 
 import setup from "../../../../lib/utils/setup";
 import trace from "../../../../lib/utils/trace";
+import getParticipation from "../../../../lib/participation/calls/getParticipation";
 import createVault from "../../../../lib/vault/transactions/createVault";
 import subscribe from "../../../../lib/participation/transactions/subscribe";
+import getOrderbook from "../../../../lib/exchange/calls/getOrderbook";
+import takeOrder from "../../../../lib/vault/transactions/takeOrder";
 
-const shared = {};
+const shared = { userBalance: {}, participation: {} };
 
 const randomString = (length = 4) =>
   Math.random().toString(36).substr(2, length);
@@ -13,20 +17,56 @@ const randomString = (length = 4) =>
 it(
   "Create fund, invest, take order, redeem",
   async () => {
+    console.log("\n");
+
+    shared.userBalance.initial = setup.web3.fromWei(
+      setup.web3.eth.getBalance(setup.defaultAccount),
+    );
+    expect(shared.userBalance.initial.toFixed()).toBeGreaterThan(3);
+
     trace({
-      message: `Start walkthrough with defaultAccount: ${setup.defaultAccount}`,
+      message: `Start walkthrough with defaultAccount: ${setup.defaultAccount}, Ξ${shared
+        .userBalance.initial}`,
       data: setup,
     });
 
     shared.vault = await createVault(`test-${randomString()}`);
-    trace({ message: `vaultCreated`, data: shared });
-
-    const txHash = await subscribe(
+    shared.participation.initial = await getParticipation(
       shared.vault.address,
-      new BigNumber(1),
-      new BigNumber(1),
+      setup.defaultAccount,
     );
+    trace({
+      message: `vaultCreated: ${shared.vault.address}`,
+      data: shared,
+    });
+    expect(shared.participation.initial.personalStake.toNumber()).toBe(0);
+    expect(shared.participation.initial.totalSupply.toNumber()).toBe(0);
+
+    await subscribe(shared.vault.address, new BigNumber(5), new BigNumber(5));
     trace({ message: `subscribed`, data: shared });
+    shared.participation.invested = await getParticipation(
+      shared.vault.address,
+      setup.defaultAccount,
+    );
+    expect(shared.participation.invested.personalStake.toNumber()).toBe(5);
+    expect(shared.participation.invested.totalSupply.toNumber()).toBe(5);
+
+    shared.orderBook = await getOrderbook("MLN-T", "ETH-T");
+    trace({
+      message: `Got orderbook for MLN-T/ETH-T with length: ${shared.orderBook
+        .length}`,
+      data: shared,
+    });
+
+    const orderToTake = findLast(propEq("type", "sell"))(shared.orderBook);
+    trace({ message: `orderToTake: ${orderToTake.id}`, data: orderToTake });
+
+    await takeOrder(
+      orderToTake.id,
+      setup.defaultAccount,
+      shared.vault.address,
+      new BigNumber(2),
+    );
   },
   10 * 60 * 1000,
 );
--- tests/integration/setup.js
+++ tests/integration/setup.js
@@ -7,7 +7,7 @@ try {
   setup.init({
     web3,
     daemonAddress: "0x00360d2b7d240ec0643b6d819ba81a09e40e5bcd",
-    defaultAccount: "0x00360d2b7d240ec0643b6d819ba81a09e40e5bcd",
+    defaultAccount: "0xfc669feb5c9a551bea36729f8f4193929a44871d",
     tracer: ({ timestamp, message, category }) => {
       console.log(timestamp.toISOString(), `[${category}]`, message);
     },
