3ed9021b0d85b4bb5e523a8c5c466de68bda5271
Jenna Zenk <zenkjenna@gmail.com>
2017-09-12 18:07:48 +0200

    Fix test for getOffer

--- tests/unit/exchange/calls/getOffer.js
+++ tests/unit/exchange/calls/getOffer.js
@@ -4,15 +4,13 @@ import getOffer from "../../../../lib/exchange/calls/getOffer";
 
 /* eslint-disable global-require */
 jest.mock("truffle-contract", () => require("../../../mocks/truffle-contract"));
-jest.mock("../../../../lib/universe/calls/getConfig", () =>
-  require("../../../mocks/truffle-contract"),
-);
+
 /* eslint-enable */
 
 test("getOffer", async () => {
   const order = await getOffer(6870);
   expect(order.sell.howMuch.eq("8.55505176")).toBeTruthy();
-  expect(contract).toHaveBeenCalledTimes(2);
+  expect(contract).toHaveBeenCalledTimes(1);
   expect(contract().setProvider).toHaveBeenCalledTimes(1);
   expect(contract.mockInspect.instance.offers).toHaveBeenCalledWith(777);
 });
