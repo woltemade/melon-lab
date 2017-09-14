8eaa1bcdd0a35ce87306cda313c7df56afaf5c97
Simon Emanuel Schmid <simon@schmid.io>
2017-09-14 14:22:13 +0200

    mechanics to enhance events params

--- tests/integration/version/events/watchVaultAssociated.js
+++ tests/integration/version/events/watchVaultAssociated.js
@@ -1,10 +1,21 @@
+import createVault from "../../../../lib/vault/transactions/createVault";
 import watchVaultAssociated from "../../../../lib/version/events/watchVaultAssociated";
+
+const randomString = (length = 4) =>
+  Math.random().toString(36).substr(2, length);
 
 it(
   "wathVaultAssociated",
   async () => {
     await new Promise(() => {
-      watchVaultAssociated("0x55a5108f08954d36560aced719716a5c5bbb9056");
+      watchVaultAssociated(
+        "0x55a5108f08954d36560aced719716a5c5bbb9056",
+        console.log,
+      );
+
+      console.log("asdf");
+      createVault(`test-${randomString()}`);
+      console.log("asdf");
     });
   },
   10 * 60 * 1000,
