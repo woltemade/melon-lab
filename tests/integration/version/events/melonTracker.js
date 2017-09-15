824e974d08f6f59f992ed82c1cc69fd22f2dcfda
Simon Emanuel Schmid <simon@schmid.io>
2017-09-15 17:39:08 +0200

    melonTracker integration tests

--- tests/integration/version/events/melonTracker.js
+++ tests/integration/version/events/melonTracker.js
@@ -1,7 +1,7 @@
 import { all } from "ramda";
 
 import setupFund from "../../../../lib/version/transactions/setupFund";
-import watchMelon from "../../../../lib/version/events/watchMelon";
+import melonTracker from "../../../../lib/version/events/melonTracker";
 
 const randomString = (length = 4) =>
   Math.random().toString(36).substr(2, length);
@@ -23,14 +23,15 @@ it(
   "watchVaultAssociated",
   async () =>
     new Promise(async resolve => {
-      watchMelon((err, eventName, args) => {
-        console.log(eventName, args);
-        eventsSeen[eventName] = args;
-        if (eventExpectations[eventName]) eventExpectations[eventName](args);
+      melonTracker.on("FundAdded", "DataUpdated")((name, args) => {
+        eventsSeen[name] = args;
+        if (eventExpectations[name]) eventExpectations[name](args);
         if (all(i => i)(Object.values(eventsSeen))) resolve();
       });
 
+      console.log("before setupFund");
       await setupFund(fundName);
+      console.log("after setupFund");
     }),
   2 * 60 * 1000,
 );
