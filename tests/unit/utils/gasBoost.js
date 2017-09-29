49a469a9b8fcdcd354b6e828bc9faa4822965918
Simon Emanuel Schmid <simon@schmid.io>
2017-09-29 12:44:44 +0200

    Enhance gasBoost to respect gasLimit

--- tests/unit/utils/gasBoost.js
+++ tests/unit/utils/gasBoost.js
@@ -1,13 +1,47 @@
 import gasBoost from "../../../lib/utils/gasBoost";
 
-const contractMethodMock = jest.fn(
-  () => new Promise(resolve => resolve({ transactionHash: "0xBLUB" })),
-);
-contractMethodMock.estimateGas = jest.fn(() => 600000);
+describe("gasBoost", () => {
+  it("boosts gas regular function", async () => {
+    const mock = jest.fn(
+      () => new Promise(resolve => resolve({ transactionHash: "0xBLUB" })),
+    );
+    mock.estimateGas = jest.fn(() => 600000);
 
-it("boosts gas", async () => {
-  const receipt = await gasBoost(contractMethodMock, [10, 5], {
-    from: "0xADDRESS",
+    const receipt = await gasBoost(mock, [10, 5], {
+      from: "0xADDRESS",
+    });
+    expect(receipt.transactionHash).toEqual("0xBLUB");
   });
-  expect(receipt.transactionHash).toEqual("0xBLUB");
+
+  it("falls back to gas limit if too high", async () => {
+    const mock = jest.fn(
+      () => new Promise(resolve => resolve({ transactionHash: "0xBLUB" })),
+    );
+    mock.estimateGas = jest.fn(() => 6900000);
+
+    const receipt = await gasBoost(mock, [], {
+      from: "0xADDRESS",
+    });
+    expect(receipt.transactionHash).toEqual("0xBLUB");
+    expect(mock).toHaveBeenCalledWith({
+      from: "0xADDRESS",
+      gas: 6972365,
+    });
+  });
+
+  it("Gas estimation is already near the gasLimit", async () => {
+    const mock = jest.fn(
+      () => new Promise(resolve => resolve({ transactionHash: "0xBLUB" })),
+    );
+    mock.estimateGas = jest.fn(() => 6972365);
+
+    const receipt = await gasBoost(mock, [], {
+      from: "0xADDRESS",
+    });
+    expect(receipt.transactionHash).toEqual("0xBLUB");
+    expect(mock).toHaveBeenCalledWith({
+      from: "0xADDRESS",
+      gas: 6972365,
+    });
+  });
 });
