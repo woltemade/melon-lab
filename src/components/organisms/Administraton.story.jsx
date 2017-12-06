8bcd4a7db79ae021821b592e83f02275fa76c939
Simon Emanuel Schmid <simon@schmid.io>
2017-12-06 10:59:50 +0100

    Add action addon

--- src/components/organisms/Administraton.story.jsx
+++ src/components/organisms/Administraton.story.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
 
 import Administration from "./Administration";
 
 storiesOf("Administration", module)
-  .add("Default", () => <Administration />)
+  .add(
+    "Default",
+    withInfo("Fund administration panel")(() => (
+      <Administration
+        loading={false}
+        subscriptionAllowed={false}
+        redemptionAllowed={false}
+        toggleRedemption={action("toggleRedemption")}
+        toggleSubscription={action("toggleSubscription")}
+        convertUnclaimedRewards={action("convertUnclaimedRewards")}
+        shutdown={action("shutdown")}
+      />
+    )),
+  )
   .add("loading", () => <Administration loading />)
   .add("subscriptionAllowed", () => <Administration subscriptionAllowed />)
   .add("redemptionAllowed", () => <Administration redemptionAllowed />);
