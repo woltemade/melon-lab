51db4b696a5ca40723607d4ee1a9f244a767e633
Simon Emanuel Schmid <simon@schmid.io>
2017-12-06 10:49:14 +0100

    Introduce info addon

--- src/components/organisms/Factsheet.story.jsx
+++ src/components/organisms/Factsheet.story.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
 import CenteredCardDecorator from "../../../tests/storybook/CenteredCardDecorator";
 
 import Factsheet from "./Factsheet";
 
 storiesOf("Factsheet", module)
   .addDecorator(CenteredCardDecorator)
-  .add("Default", () => (
-    <Factsheet
-      creationDate="1. Dec 2017 12:12"
-      aum="12.1234"
-      name="Storybook Fund"
-      managementReward="0.0000"
-      performanceReward="0.0000"
-      sharePrice="1.0000"
-      totalSupply="1.0000"
-    />
-  ));
+  .add(
+    "Default",
+    withInfo("Displays the Factsheet of a fund")(() => (
+      <Factsheet
+        creationDate="1. Dec 2017 12:12"
+        aum="12.1234"
+        name="Storybook Fund"
+        managementReward="0.0000"
+        performanceReward="0.0000"
+        sharePrice="1.0000"
+        totalSupply="1.0000"
+      />
+    )),
+  );
--- tests/storybook/config.js
+++ tests/storybook/config.js
@@ -1,11 +1,16 @@
 import { configure, addDecorator } from "@storybook/react";
+import { setDefaults } from "@storybook/addon-info";
 import ContainerDecorator from "./ContainerDecorator";
 import "../../public/semantic/dist/semantic.min.css";
 import "../../src/index.css";
 
-const req = require.context("../../src/", true, /\.story\.jsx$/);
+setDefaults({
+  header: false,
+});
 
 addDecorator(ContainerDecorator);
+
+const req = require.context("../../src/", true, /\.story\.jsx$/);
 
 function loadStories() {
   req.keys().forEach(req);
