7c72fc0513af17ff2e7672ea87ab815157894b02
Jenna Zenk <zenkjenna@gmail.com>
2017-12-18 18:17:30 +0100

    Onboarded state displays fund view for manager

--- src/components/pages/App.jsx
+++ src/components/pages/App.jsx
@@ -22,6 +22,7 @@ const mapOnboardingStateToMainContainer = {
   [onboardingPath.INSUFFICENT_MLN]: InsufficentFunds,
   [onboardingPath.NO_FUND_CREATED]: SetupContainer,
   [onboardingPath.NOT_INVESTED_IN_OWN_FUND]: ParticipationContainer,
+  [onboardingPath.ONBOARDED]: FundContainer,
 };
 
 const routeContainerMap = {
@@ -30,7 +31,6 @@ const routeContainerMap = {
   [types.ACCOUNT_RESTORE]: RestoreContainer,
   [types.ACCOUNT_CREATE]: AccountContainer,
   [types.ACCOUNT_ENCRYPT]: AccountContainer,
-  // [types.ACCOUNT_SETUP]: AccountContainer,
   [types.FUND]: FundContainer,
 };
 
@@ -38,12 +38,13 @@ const getMainComponent = ({
   onboardingState,
   mlnBalance,
   ethBalance,
+  usersFund,
   route,
 }) => {
   if (route === types.SETUP) {
     const Main = mapOnboardingStateToMainContainer[onboardingState];
     return Main ? (
-      <Main mlnBalance={mlnBalance} ethBalance={ethBalance} setup />
+      <Main mlnBalance={mlnBalance} ethBalance={ethBalance} setup usersFund />
     ) : null;
   }
   const Main = routeContainerMap[route];
--- src/components/pages/Fund.jsx
+++ src/components/pages/Fund.jsx
@@ -3,22 +3,22 @@ import { Card } from "semantic-ui-react";
 
 import Factsheet from "../../containers/Factsheet";
 import Holdings from "../../containers/Holdings";
-// import Administration from "../../containers/Administration";
+import Administration from "../../containers/Administration";
 import Orderbook from "../../containers/Orderbook";
 import RecentTrades from "../../containers/RecentTrades";
 import TradeHistory from "../../containers/TradeHistory";
 import GetStarted from "../organisms/GetStarted";
 import Participation from "../../containers/Participation";
 
-const ManagerView = ({ isVisitor, isInvestor, isManager, fundAddress }) => (
+const Fund = ({ isVisitor, isInvestor, isManager, fundAddress }) => (
   <div className="App">
     <br />
     <div>
       <Card.Group>
         <Factsheet />
-        {/* {isManager ? <Administration /> : <div />} */}
+        {isManager ? <Administration /> : <div />}
         {isVisitor ? <GetStarted /> : <div />}
-        {isInvestor ? <Participation /> : <div />}
+        {isInvestor || isManager ? <Participation /> : <div />}
       </Card.Group>
       <br />
       <Holdings address={fundAddress} />
@@ -33,4 +33,4 @@ const ManagerView = ({ isVisitor, isInvestor, isManager, fundAddress }) => (
   </div>
 );
 
-export default ManagerView;
+export default Fund;
