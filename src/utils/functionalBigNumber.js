3cc8b86b63cb885ec425b2f2e65f309f2326bbea
Simon Emanuel Schmid <simon@schmid.io>
2018-01-10 18:25:11 +0100

    Fix shut down fund. Closes #161

--- src/utils/functionalBigNumber.js
+++ src/utils/functionalBigNumber.js
@@ -1,15 +1,25 @@
 import BigNumber from "bignumber.js";
 
-export const toBigNumber = value => new BigNumber(value || 0);
+export const toBigNumber = value =>
+  new BigNumber(value === "..." ? 0 : value || 0);
 
 export const multiply = (base, ...args) =>
-  args.reduce((acc, current) => acc.times(current), toBigNumber(base));
+  args.reduce(
+    (acc, current) => acc.times(toBigNumber(current)),
+    toBigNumber(base),
+  );
 
 export const add = (base, ...args) =>
-  args.reduce((acc, current) => acc.add(current), toBigNumber(base));
+  args.reduce(
+    (acc, current) => acc.add(toBigNumber(current)),
+    toBigNumber(base),
+  );
 
 export const divide = (base, ...args) =>
-  args.reduce((acc, current) => acc.div(current), toBigNumber(base));
+  args.reduce(
+    (acc, current) => acc.div(toBigNumber(current)),
+    toBigNumber(base),
+  );
 
 export const isZero = number => toBigNumber(number).eq(0);
 
