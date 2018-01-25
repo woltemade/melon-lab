93bfc48cacd9085db3ac5609d5c38e667c079a8e
Simon Emanuel Schmid <simon@schmid.io>
2018-01-25 12:48:48 +0100

    Use grid instead of tabels. Fixes #116
    To fix safari orderbook display bug

--- src/components/organisms/Orderbook.jsx
+++ src/components/organisms/Orderbook.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import BigNumber from "bignumber.js";
-import { Table, Grid, Image } from "semantic-ui-react";
+import { Grid, Image } from "semantic-ui-react";
 
-const getSellGradient = (cumulativeVolume, totalVolume) => {
+const getBuyRowStyle = (
+  cumulativeVolume,
+  totalVolume,
+  isReadyToTrade,
+  isLast,
+) => {
   const percentage = new BigNumber(cumulativeVolume)
     .div(totalVolume)
     .times(100);
   const reverse = new BigNumber(100).minus(percentage);
   return {
     background: `linear-gradient(to right, rgba(201, 88, 88,0) 0%, rgba(201, 88, 88,0) ${reverse}%, rgba(201, 88, 88,0.3) ${reverse}%, rgba(201, 88, 88,0.3) 100%)`,
+    cursor: isReadyToTrade ? "pointer" : "auto",
+    padding: 0,
+    borderBottom: isLast ? "none" : "1px solid rgba(34, 36, 38, 0.15)",
   };
 };
 
-const getBuyGradient = (cumulativeVolume, totalVolume) => {
+const get_SellRowStyle = (
+  cumulativeVolume,
+  totalVolume,
+  isReadyToTrade,
+  isLast,
+) => {
   const percentage = new BigNumber(cumulativeVolume)
     .div(totalVolume)
     .times(100);
   return {
     background: `linear-gradient(to right, rgba(71, 161, 71,0.3) 0%, rgba(71, 161, 71,0.3) ${percentage}%, rgba(71, 161, 71,0) ${percentage}%, rgba(71, 161, 71,0) 100%)`,
+    cursor: isReadyToTrade ? "pointer" : "auto",
+    padding: 0,
+    borderBottom: isLast ? "none" : "1px solid rgba(34, 36, 38, 0.15)",
   };
 };
 
@@ -27,6 +43,36 @@ const onMouseOver = e => {
 
 const onMouseOut = e => {
   e.currentTarget.style.backgroundColor = "initial";
+};
+
+const getRowInteraction = (order, onClick, isReadyToTrade) => ({
+  key: order.id,
+  onMouseOver,
+  onMouseOut,
+  onFocus: onMouseOver,
+  onBlur: onMouseOut,
+  onClick: () => (isReadyToTrade ? onClick(order.id) : null),
+});
+
+const tableBuyHeadCellStyle = {
+  fontWeight: "bold",
+  marginTop: 5,
+  marginBottom: 5,
+};
+
+const tableSellHeadCellStyle = {
+  ...tableBuyHeadCellStyle,
+  textAlign: "right",
+};
+
+const tableBuyCellStyle = {
+  marginTop: 10,
+  marginBottom: 10,
+};
+
+const tableSellCellStyle = {
+  ...tableBuyCellStyle,
+  textAlign: "right",
 };
 
 const Orderbook = ({
@@ -49,93 +95,86 @@ const Orderbook = ({
         <Image src="./melon-spinner.gif" size="small" centered />
       </div>
     ) : (
-      <Grid padded={false}>
-        <Grid.Row columns={2}>
-          <Grid.Column style={{ paddingRight: 0 }}>
-            <Table>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell style={{ textAlign: "right" }}>
-                    Cum. Vol.
-                  </Table.HeaderCell>
-                  <Table.HeaderCell style={{ textAlign: "right" }}>
-                    Vol.
-                  </Table.HeaderCell>
-                  <Table.HeaderCell style={{ textAlign: "right" }}>
-                    Bid
-                  </Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body
-                style={{ cursor: isReadyToTrade ? "pointer" : "auto" }}
+      <Grid padded={false} container columns={2} style={{ marginTop: 30 }}>
+        <Grid.Column id="SellOrders">
+          <Grid padded={false}>
+            <Grid.Row
+              columns={3}
+              style={{
+                padding: 0,
+                borderBottom: "1px solid rgba(34, 36, 38, 0.15)",
+              }}
+            >
+              <Grid.Column style={tableSellHeadCellStyle}>
+                Cum. Vol.
+              </Grid.Column>
+              <Grid.Column style={tableSellHeadCellStyle}>Vol.</Grid.Column>
+              <Grid.Column style={tableSellHeadCellStyle}>Bid</Grid.Column>
+            </Grid.Row>
+            {buyOrders.map((order, index, { length }) => (
+              <Grid.Row
+                container
+                style={getBuyRowStyle(
+                  order.cumulativeVolume,
+                  totalSellVolume,
+                  isReadyToTrade,
+                  index + 1 === length,
+                )}
+                {...getRowInteraction(order, onClick, isReadyToTrade)}
+                columns={3}
               >
-                {buyOrders.map(order => (
-                  <Table.Row
-                    id="SellOrders"
-                    key={order.id}
-                    style={getSellGradient(
-                      order.cumulativeVolume,
-                      totalSellVolume,
-                    )}
-                    onMouseOver={onMouseOver}
-                    onMouseOut={onMouseOut}
-                    onClick={() => {
-                      if (isReadyToTrade) onClick(order.id);
-                    }}
-                  >
-                    <Table.Cell style={{ textAlign: "right" }}>
-                      {order.cumulativeVolume}
-                    </Table.Cell>
-                    <Table.Cell style={{ textAlign: "right" }}>
-                      {order.howMuch}
-                    </Table.Cell>
-                    <Table.Cell style={{ textAlign: "right" }}>
-                      {order.price}
-                    </Table.Cell>
-                  </Table.Row>
-                ))}
-              </Table.Body>
-            </Table>
-          </Grid.Column>
-          <Grid.Column style={{ paddingLeft: 0, marginLeft: -1 }}>
-            <Table>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>
-                    &nbsp;&nbsp;&nbsp;&nbsp;Ask
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>Vol.</Table.HeaderCell>
-                  <Table.HeaderCell>Cum. Vol.</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body
-                style={{ cursor: isReadyToTrade ? "pointer" : "auto" }}
+                <Grid.Column style={tableSellCellStyle}>
+                  {order.cumulativeVolume}
+                </Grid.Column>
+                <Grid.Column style={tableSellCellStyle}>
+                  {order.howMuch}
+                </Grid.Column>
+                <Grid.Column style={tableSellCellStyle}>
+                  {order.price}
+                </Grid.Column>
+              </Grid.Row>
+            ))}
+          </Grid>
+        </Grid.Column>
+        <Grid.Column id="BuyOrders" style={{ marginLeft: -1 }}>
+          <Grid padded={false}>
+            <Grid.Row
+              columns={3}
+              style={{
+                padding: 0,
+                borderBottom: "1px solid rgba(34, 36, 38, 0.15)",
+              }}
+            >
+              <Grid.Column style={tableBuyHeadCellStyle}>Ask</Grid.Column>
+              <Grid.Column style={tableBuyHeadCellStyle}>Vol.</Grid.Column>
+              <Grid.Column style={tableBuyHeadCellStyle}>Cum. Vol.</Grid.Column>
+            </Grid.Row>
+
+            {sellOrders.map((order, index, { length }) => (
+              <Grid.Row
+                container
+                style={get_SellRowStyle(
+                  order.cumulativeVolume,
+                  totalBuyVolume,
+                  isReadyToTrade,
+                  index + 1 === length,
+                )}
+                {...getRowInteraction(order, onClick, isReadyToTrade)}
+                columns={3}
               >
-                {sellOrders.map(order => (
-                  <Table.Row
-                    id="BuyOrders"
-                    key={order.id}
-                    style={getBuyGradient(
-                      order.cumulativeVolume,
-                      totalBuyVolume,
-                    )}
-                    onMouseOver={onMouseOver}
-                    onMouseOut={onMouseOut}
-                    onClick={() => {
-                      if (isReadyToTrade) onClick(order.id);
-                    }}
-                  >
-                    <Table.Cell>
-                      &nbsp;&nbsp;&nbsp;&nbsp;{order.price}
-                    </Table.Cell>
-                    <Table.Cell>{order.howMuch}</Table.Cell>
-                    <Table.Cell>{order.cumulativeVolume}</Table.Cell>
-                  </Table.Row>
-                ))}
-              </Table.Body>
-            </Table>
-          </Grid.Column>
-        </Grid.Row>
+                <Grid.Column style={tableBuyCellStyle}>
+                  {order.price}
+                </Grid.Column>
+                <Grid.Column style={tableBuyCellStyle}>
+                  {order.howMuch}
+                </Grid.Column>
+                <Grid.Column style={tableBuyCellStyle}>
+                  {order.price}
+                </Grid.Column>
+              </Grid.Row>
+            ))}
+          </Grid>
+        </Grid.Column>
       </Grid>
     )}
   </div>
