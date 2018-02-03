cb944d9bd8d35e929e7e8f33369a8b741545cfbd
Jenna Zenk <zenkjenna@gmail.com>
2018-02-03 18:28:09 +0100

    Fix expected share price. Prize only for top 100

--- src/sagas/ranking.js
+++ src/sagas/ranking.js
@@ -2,7 +2,7 @@ import { takeLatest, call, put, select, take } from "redux-saga/effects";
 import { actions, types } from "../actions/ranking";
 import { types as ethereumTypes } from "../actions/ethereum";
 import { types as routeTypes } from "../actions/routes";
-import { add, greaterThan, divide } from "../utils/functionalBigNumber";
+import { add, greaterThan, divide, equals } from "../utils/functionalBigNumber";
 // import rankingMock from "../utils/mocks/ranking.json";
 
 function getRanking() {
@@ -23,20 +23,28 @@ function* getRankingSaga() {
       sharePrice: fund.sharePrice.toString(),
     }));
     const filteredRanking = bigNumberifyRanking
-      .sort((a, b) => (greaterThan(a.sharePrice, b.sharePrice) ? -1 : 1))
+      .sort((a, b) => {
+        if (equals(a.sharePrice, 1) && equals(b.sharePrice, 1))
+          return a.inception < b.inception ? -1 : 1;
+        return greaterThan(a.sharePrice, b.sharePrice) ? -1 : 1;
+      })
       .filter(fund => fund.isCompeting);
     const nonCompeting = bigNumberifyRanking.filter(fund => !fund.isCompeting);
-    const sumTop100Registered = filteredRanking.reduce(
+    const top100 = filteredRanking.slice(0, 100);
+    const nontop100 = filteredRanking.slice(100, filteredRanking.length);
+    const sumTop100Registered = top100.reduce(
       (acc, fund) => add(acc, fund.sharePrice),
       0,
     );
-    const competing = filteredRanking.map(fund => ({
+    const top100WithExpectedPrize = top100.map(fund => ({
       ...fund,
       expectedPrize: divide(fund.sharePrice, sumTop100Registered).times(550),
     }));
-    const finalRanking = [...nonCompeting, ...competing].sort(
-      (a, b) => (greaterThan(a.sharePrice, b.sharePrice) ? -1 : 1),
-    );
+    const finalRanking = [
+      ...nonCompeting,
+      ...top100WithExpectedPrize,
+      ...nontop100,
+    ].sort((a, b) => (greaterThan(a.sharePrice, b.sharePrice) ? -1 : 1));
     const withRank = finalRanking.map((fund, i) => ({
       ...fund,
       rank: i + 1,
