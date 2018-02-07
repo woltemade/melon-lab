b34e510f884edaad831f9ab8274770413a370e85
Jenna Zenk <zenkjenna@gmail.com>
2018-02-07 10:34:23 +0100

    Integration test pass with parity signer

--- tests/integration/setup.js
+++ tests/integration/setup.js
@@ -6,15 +6,12 @@ import Api from '@parity/api';
 import setup from '../../lib/utils/setup';
 /* eslint-enable */
 
-// const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-const provider = new Api.Provider.Http('https://kovan.melonport.com');
+const provider = new Api.Provider.Http('http://localhost:8545');
 const api = new Api(provider);
 
 try {
   setup.init({
     provider,
-    daemonAddress: '0x00360d2b7d240ec0643b6d819ba81a09e40e5bcd',
-    defaultAccount: '0x2809a8B74D51eEcFE4d229a47937964e43D55c30',
     tracer: ({ timestamp, message, category, data }) => {
       const args = [timestamp.toISOString(), `[${category}]`, message];
       if (category === 'ensureFailed') {
