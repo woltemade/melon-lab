eb53016da23cd68f76b38269ab590f630dfc407b
Simon Emanuel Schmid <simon@schmid.io>
2018-02-23 14:56:57 +0100

    Avoid infinite loop by populating sync config before async one

--- tests/scratchpad/files/same-structure/main.js
+++ tests/scratchpad/files/same-structure/main.js
@@ -9,16 +9,18 @@ it('Scratchpad', async () => {
   console.log('Starting scratchpad ... \n\n');
   const environment = await getParityProvider(-1);
 
-  // console.log({ environment });
+  console.log(environment.providerType);
+
+  const config = await getConfig(environment);
+  console.log(config);
 
   const result = await getQuoteAssetSymbol(environment);
+
+  console.log(result);
 
   const price = await getPrice(environment, result);
 
   const rcp = await hasRecentPrice(environment);
 
   console.log(result, price.toString(), rcp);
-
-  const config = await getConfig(environment);
-  console.log(config);
 });
