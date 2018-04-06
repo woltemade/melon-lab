171e1cd3ba23f82536a224181c1c9674c0313d62
Simon Emanuel Schmid <simon@schmid.io>
2018-04-06 17:32:44 +0200

    Subscribe to prices & custom scalar

--- packages/graphql-server/index.js
+++ packages/graphql-server/index.js
@@ -1,4 +1,6 @@
+import { GraphQLScalarType, Kind } from 'graphql';
 import { GraphQLServer, PubSub } from 'graphql-yoga';
+import { getParityProvider, getPrice } from '@melonproject/melon.js';
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 // eslint-ignore
@@ -10,12 +12,34 @@ const typeDefs = `
     hello(name: String): String!
   }
 
+  scalar Symbol
+
   type Subscription {
     timer: String!
+    price(symbol: Symbol!): String!
   }
 `;
 
 const resolvers = {
+  Symbol: new GraphQLScalarType({
+    name: 'Symbol',
+    parseValue(value) {
+      return value;
+    },
+    serialize(value) {
+      return value.toString();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.STRING) {
+        if (ast.value.length > 10) {
+          throw new TypeError('Literal: Symbols are shorter than 6 chars');
+        } else {
+          return ast.value.toString();
+        }
+      }
+      return null;
+    },
+  }),
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
@@ -30,6 +54,19 @@ const resolvers = {
             }),
           1000,
         );
+        return context.pubSub.asyncIterator(channel);
+      },
+    },
+    price: {
+      subscribe: (parent, args, context) => {
+        const channel = `price:${args.symbol}`;
+        setInterval(async () => {
+          const environment = await getParityProvider();
+          const price = await getPrice(environment, args.symbol);
+          context.pubSub.publish(channel, {
+            price,
+          });
+        }, 1000);
         return context.pubSub.asyncIterator(channel);
       },
     },
