ea4f1cb9469fb280aacb26ddfa2f5e6f99ebe982
tresiwald <andreas.waldis@live.com>
2018-05-18 15:53:06 +0200

    Update getRequestsHistory.js
    Update Function Signature for supporting environment

--- packages/melon.js/lib/fund/calls/getRequestsHistory.js
+++ packages/melon.js/lib/fund/calls/getRequestsHistory.js
@@ -6,8 +6,8 @@ import type { Address } from '../../assets/schemas/Address';
 /**
  * Get all the subscribe/redeem requests this fund at `fundAddress` received so far
  */
-const getRequestsHistory = async (fundAddress: Address): Promise<any> => {
-  const fundContract = getFundContract(fundAddress);
+const getRequestsHistory = async (enviroment, fundAddress: Address): Promise<any> => {
+  const fundContract = getFundContract(environment, fundAddress);
 
   const requests = await fundContract.instance.requests.call();
 
