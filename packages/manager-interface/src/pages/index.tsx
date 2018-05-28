7a538b0256c315df11ffc3a0b63e114f345b72af
Sebastian Siemssen <fubhy@fubhy.com>
2018-05-28 13:59:24 +0200

    Remove loading.

--- packages/manager-interface/src/pages/index.tsx
+++ packages/manager-interface/src/pages/index.tsx
@@ -1,12 +1,4 @@
-import dynamic from 'next/dynamic';
 import withApollo from '~/shared/withApollo';
-
 import legacy from '../legacy';
 
-// console.log(legacy);
-
-const App = dynamic(import('../legacy/index'), {
-  ssr: false,
-});
-
-export default withApollo(App);
+export default withApollo(legacy);
