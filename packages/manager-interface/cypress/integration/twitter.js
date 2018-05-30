fe112b06069a22477cd2e55be4978ba6bf53cc30
Simon Emanuel Schmid <simon@schmid.io>
2018-05-30 18:30:53 +0200

    Remove cypress

--- packages/manager-interface/cypress/integration/twitter.js
+++
@@ -1,7 +0,0 @@
-import wallet from "../fixtures/walletReadyToTrade.json";
-
-it("There is a twitter button", () => {
-  cy.visit("http://localhost:3000/");
-  window.localStorage.setItem("wallet:melon.fund", JSON.stringify(wallet));
-  cy.get("[href*='https://twitter.com/intent/tweet']").should("be.visible");
-});
