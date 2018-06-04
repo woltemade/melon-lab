6c9129eb148ae60c12b68d6df2715137035f5154
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-06-04 10:44:25 +0200

    Fix colors import

--- packages/manager-components/src/styles/variables.js
+++ packages/manager-components/src/styles/variables.js
@@ -1,6 +1,7 @@
 module.exports = {
   ...require('./spacings.js'),
-  ...require('./colors.js'),
+  ...require('./colors.js').mainColors,
+  ...require('./colors.js').statusColors,
   ...require('./typography.js').fontFamilies,
   ...require('./typography.js').fontSizes,
   ...require('./typography.js').fontWeights,
