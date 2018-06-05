74814cc1bfcee99456cf40ff3e867267a0946b45
Sebastian Siemssen <fubhy@fubhy.com>
2018-06-05 10:11:14 +0200

    Removing electron-builder fork.

--- builder/scripts/snap-template.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ~/squashfs-root
-unsquashfs /media/psf/test/electron-builder-test/dist/se-wo-template_1.1.0_amd64.snap
-rm -rf ~/squashfs-root/app ~/squashfs-root/snap ~/squashfs-root/meta ~/squashfs-root/command.sh
-
-rm -f /home/develar/snap-template-electron-2.3.tar.7z && cd ~/squashfs-root && tar cf - . | 7za a -mx=9 -mfb=64 -si /home/develar/snap-template-electron-2.3.tar.7z
-
-shasum -a 512 /Volumes/test/electron-template-2.3.tar.7z | xxd -r -p | base64
